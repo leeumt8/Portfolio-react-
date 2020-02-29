ab92a2ac5134c7504476316fe1c1c295bad7e117
leeum <liam.tate@hotmail.com>
2020-02-29 06:14:58 -0500

    finalized react portfolio

---
+++ src/resumeData.js
@@ -0,0 +1,92 @@
+let resumeData = {
+    "imagebaseurl":"https://github.com/leeumt8",
+    "name": "Liam Tate",
+    "role": "Fullstack Web Developer",
+    "linkedinId":"https://www.linkedin.com/in/liam-tate-ba5609190/",
+    "skypeid": "Your skypeid",
+    "roleDescription": " I am junior level web developer looking for opportunities to help flesh out my portfolio, as well as getting hands on experience.",
+    "socialLinks":[
+        {
+          "name":"linkedin",
+          "url":"https://www.linkedin.com/in/liam-tate-ba5609190/",
+          "className":"fa fa-linkedin"
+        },
+        {
+          "name":"github",
+          "url":"https://github.com/leeumt8",
+          "className":"fa fa-github"
+        },
+        {
+          "name":"skype",
+          "url":"",
+          "className":"fa fa-twitter"
+        }
+      ],
+    "aboutme":"I spend most of my current free time continuing to hone my dev skills, but if possible I try to squeeze in some poker and video games.",
+    "address":"New Smyrna Beach",
+    "website":"https://github.com/leeumt8",
+    "education":[
+      {
+        "UniversityName":"St. Edward's University",
+        "specialization":"GEN-EDS",
+        "MonthOfPassing":"Dec",
+        "YearOfPassing":"2018",
+        "Achievements":"Dean's List from 2017-2018"
+      }
+    ],
+    "work":[
+      {
+        "CompanyName":"Third Wave Cafe",
+        "specialization":"Server",
+        "MonthOfLeaving":"TBD",
+        "YearOfLeaving":"TBD",
+        "Achievements":"Single&Double Shift Sales Records"
+      }
+    ],
+    "skillsDescription":"I have a basic understanding of the core languages/libraries found in web development.",
+    "skills":[
+      {
+        "skillname":"MERN"
+      },
+      {
+        "skillname":"mySQL"
+      },
+      {
+        "skillname":"React Native"
+      }
+    ],
+    "portfolio":[
+      {
+        "name":"Bamazon",
+        "description":"Bamazon is a Node.js CLI App that behaves similarly to a marketplace.",
+        "imgurl":"images/portfolio/bama.png"
+      },
+      {
+        "name":"Pokepedia",
+        "description":"Pokepedia acts as a helper for the iconic Emerald Version of the Pokemon franchise.",
+        "imgurl":"images/portfolio/pokepedia.png"
+      },
+      {
+        "name":"Train Scheduler",
+        "description":"Train Scheduler is a Node.js App that focuses on mySQL relations.",  
+        "imgurl":"images/portfolio/trainscheduler.png"
+      },
+      {
+        "name":"LIRI",
+        "description":"LIRI is a bot that allows users to search APIs through the CLI.",
+        "imgurl":"images/portfolio/LIRI.png"
+      }
+    ],
+    "testimonials":[
+      {
+        "description":"TBA",
+        "name":"Someone important"
+      },
+      {
+        "description":"TBA",
+        "name":"Someone important"
+      }
+    ]
+  }
+  
+  export default resumeData
